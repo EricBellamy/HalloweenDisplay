ea7f94cc988558b6fce7fc2ffc1d292104c9a7de
Eric Bellamy <ericbellamy@rogers.com>
2022-10-03 04:24:44 -0400

    basic canvas CSS

--- frontend/includes/js/canvas.js
+++ frontend/includes/js/canvas.js
@@ -1,7 +1,15 @@
 const canvas = document.querySelector("canvas");
-const scale = Math.min(
-	Math.floor(window.innerWidth / 300),
-	Math.floor(window.innerHeight / 300)
-);
 
-console.log(scale);
+const canvasPadding = 20;
+const canvasResolution = 300;
+const scale = Math.min(
+	Math.floor((window.innerWidth - canvasPadding) / 300),
+	Math.floor((window.innerHeight - canvasPadding) / 300)
+);
+const canvasSize = canvasResolution * scale;
+
+canvas.width = canvasSize;
+canvas.style.width = `${canvasSize}px`;
+canvas.height = canvasSize;
+canvas.style.height = `${canvasSize}px`;
+
