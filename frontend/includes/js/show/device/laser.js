e3f44639d8e13bb64322e015279631973a97f869
Eric Bellamy <ericbellamy@rogers.com>
2022-10-18 18:34:48 -0400

    better laser popup

--- frontend/includes/js/show/device/laser.js
+++ frontend/includes/js/show/device/laser.js
@@ -1,30 +1,18 @@
 const laserInput = window.device.addInput("laserPopup");
 
-laserInput.element = tired.html.create(`
-<div class="hidden" id="laserPopup">
-	<select name="designs" id="laser-select">
-		<option disabled selected value> -- select an option -- </option>
-		<option value="shuriken">Shuriken</option>
-		<option value="lollipop">Lollipop</option>
-		<option value="rock">Rock</option>
-		<option value="hexagon">Hexagon</option>
-	</select>
-</div>`);
-
+laserInput.element = window.laserDisplay.element;
 laserInput.elementParts = tired.html.parse(laserInput.element);
-laserInput.deactivate = function () {
-	console.log("DEACTIVATING!");
+laserInput.deactivate = function (close = false) {
 	this.element.classList.toggle("hidden", true);
+
+	// If we just need to close this window
+	if(close) return;
 
 	// Create / Update the event here
 	window.timeline.addEvent(laserInput.current.value);
 }
 laserInput.activate = function (beatX, beatY, beatBounds, device) {
-	console.log("ACTIVATE THE LASER SELECT HERE!");
-
 	this.element.classList.toggle("hidden", false);
-	this.element.style.left = `${beatBounds.x}px`;
-	this.element.style.top = `${beatBounds.y}px`;
 }
 
 laserInput.render = function (device, value) {
@@ -34,29 +22,8 @@ laserInput.unRender = function(device){
 	window.laserDisplay.clearCanvas(device.name);
 }
 
-// Listen for select
-laserInput.elementParts[0][0].addEventListener('change', function () {
-	laserInput.setValue(this.value);
-	laserInput.deactivate();
-	this.selectedIndex = 0;
-});
-
 document.body.appendChild(laserInput.element);
 window.popups.laserPopup = laserInput;
-
-
-
-
-
-
-window.laserDisplay.registerDesign("shuriken", "http://localhost:3000/points.html?points=0f0%2C30%2C18%2C0f0%2C60%2C0%2C00f%2C42%2C30%2C00f%2C60%2C60%2Cff0%2C30%2C42%2Cff0%2C0%2C60%2Cf00%2C18%2C30%2Cf00%2C0%2C0");
-window.laserDisplay.registerDesign("lollipop", "http://localhost:3000/points.html?points=fff%2C28%2C30%2Cfff%2C28%2C43%2Cfff%2C32%2C43%2Cfff%2C32%2C30%2Cfff%2C36%2C27%2Cfff%2C39%2C23%2Cfff%2C39%2C18%2Cfff%2C36%2C14%2Cfff%2C32%2C11%2Cfff%2C28%2C11%2Cfff%2C24%2C14%2Cfff%2C21%2C18%2Cfff%2C21%2C23%2Cfff%2C24%2C27");
-window.laserDisplay.registerDesign("rock", "?points=ffffff%2C20%2C8%2Cffffff%2C45%2C9%2Cffffff%2C54%2C17%2Cffffff%2C53%2C33%2Cffffff%2C53%2C50%2Cffffff%2C40%2C56%2Cffffff%2C24%2C57%2Cffffff%2C14%2C54%2Cffffff%2C11%2C42%2Cffffff%2C7%2C31%2Cffffff%2C7%2C22%2Cffffff%2C9%2C17%2Cffffff%2C14%2C12%2Cffffff%2C17%2C10");
-window.laserDisplay.registerDesign("hexagon", "http://localhost:3000/points.html?points=fff%2C10%2C20%2Cfff%2C20%2C10%2Cfff%2C40%2C10%2Cfff%2C50%2C20%2Cfff%2C50%2C40%2Cfff%2C40%2C50%2Cfff%2C20%2C50%2Cfff%2C10%2C40");
-
-
-
-
 
 
 
@@ -67,6 +34,14 @@ window.device.addDevice("laser", "laserPopup", document.querySelector("#laser-1"
 window.device.addDevice("laser", "laserPopup", document.querySelector("#laser-2"));
 window.device.addDevice("laser", "laserPopup", document.querySelector("#laser-3"));
 
+// Register the canvases
 window.laserDisplay.registerCanvas("laser-1", document.querySelector("#laser-1-canvas"));
 window.laserDisplay.registerCanvas("laser-2", document.querySelector("#laser-2-canvas"));
 window.laserDisplay.registerCanvas("laser-3", document.querySelector("#laser-3-canvas"));
+
+// Register the designs
+window.laserDisplay.registerDesign("shuriken", "http://localhost:3000/points.html?points=0f0%2C30%2C18%2C0f0%2C60%2C0%2C00f%2C42%2C30%2C00f%2C60%2C60%2Cff0%2C30%2C42%2Cff0%2C0%2C60%2Cf00%2C18%2C30%2Cf00%2C0%2C0");
+window.laserDisplay.registerDesign("lollipop", "http://localhost:3000/points.html?points=fff%2C28%2C30%2Cfff%2C28%2C43%2Cfff%2C32%2C43%2Cfff%2C32%2C30%2Cfff%2C36%2C27%2Cfff%2C39%2C23%2Cfff%2C39%2C18%2Cfff%2C36%2C14%2Cfff%2C32%2C11%2Cfff%2C28%2C11%2Cfff%2C24%2C14%2Cfff%2C21%2C18%2Cfff%2C21%2C23%2Cfff%2C24%2C27");
+window.laserDisplay.registerDesign("rock", "?points=ffffff%2C20%2C8%2Cffffff%2C45%2C9%2Cffffff%2C54%2C17%2Cffffff%2C53%2C33%2Cffffff%2C53%2C50%2Cffffff%2C40%2C56%2Cffffff%2C24%2C57%2Cffffff%2C14%2C54%2Cffffff%2C11%2C42%2Cffffff%2C7%2C31%2Cffffff%2C7%2C22%2Cffffff%2C9%2C17%2Cffffff%2C14%2C12%2Cffffff%2C17%2C10");
+window.laserDisplay.registerDesign("hexagon", "http://localhost:3000/points.html?points=fff%2C10%2C20%2Cfff%2C20%2C10%2Cfff%2C40%2C10%2Cfff%2C50%2C20%2Cfff%2C50%2C40%2Cfff%2C40%2C50%2Cfff%2C20%2C50%2Cfff%2C10%2C40");
+window.laserDisplay.registerDesign("star", "?points=fff%2C30%2C6%2Cfff%2C40%2C22%2Cfff%2C55%2C23%2Cfff%2C43%2C35%2Cfff%2C49%2C53%2Cfff%2C30%2C42%2Cfff%2C11%2C53%2Cfff%2C17%2C35%2Cfff%2C5%2C23%2Cfff%2C20%2C22");
--- frontend/includes/js/show/laser-display.js
+++ frontend/includes/js/show/laser-display.js
@@ -4,11 +4,30 @@ class LaserDisplayManager {
 
 	designs = {};
 	canvases = {};
-	constructor() {
 
+	element = tired.html.create(`
+	<div class="hidden" id="laserPopup">
+		<div id="laserPopupHeader">
+			<div>
+				<div id="laserClose">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z" fill="currentColor" />
+					</svg>
+				</div>
+				<div>Close</div>
+			</div>
+		</div>
+		<div id="laserPopupWrapper"></div>
+	</div>`);
+
+	constructor() {
+		this.elementParts = tired.html.parse(this.element);
+		this.elementParts[0][0].addEventListener("click", function(){
+			laserInput.deactivate(true);
+		});
 	}
 
-	drawLine(context, startX, startY, endX, endY, color, debug = false) {
+	drawLine(context, startX, startY, endX, endY, color = "ffffff", debug = false) {
 		startX *= this.downscaleFactor;
 		startY *= this.downscaleFactor;
 		endX *= this.downscaleFactor;
@@ -66,6 +85,13 @@ class LaserDisplayManager {
 	}
 
 	registerDesign(name, URL) {
+		const newOptionElement = tired.html.create(`<div class="laser-popup-option" data-value="${name}">${name}</div>`);
+		newOptionElement.addEventListener("click", function () {
+			laserInput.setValue(this.dataset.value);
+			laserInput.deactivate();
+		});
+		this.element.querySelector("#laserPopupWrapper").appendChild(newOptionElement);
+
 		const newLaserDisplay = {
 			URL: URL,
 			len: 0,
